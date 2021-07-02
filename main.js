07b15821d6812e19cc42b65b9f844526575d7b3a
Olasunkanmi Fafowora <sunkanmifafowora@gmail.com>
2021-07-02 23:45:37 +0100

    done with date and time

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
-function checkInput(){
-    
+function checkInput() {
+
 }
+
+function getCurrentDate() {
+    let today = new Date();
+    let date = `${(today.getMonth() + 1)}/${today.getDate()}/${today.getFullYear()}`;
+    let hours = today.getHours();
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    let time = `${hours}:${today.getMinutes()} ${ampm}`;
+    let machineDate = `${today.getFullYear()}-${(today.getMonth() + 1)}-${today.getDate()}`;
+    let machineTime = `${today.getHours}:${today.getMinutes()}`;
+    let dateTime = {
+        timeTag: {date, time},
+        machineReadableDate: {machineDate, machineTime}
+    };
+    return dateTime;
+}
+
+(function addDateToDOM(){
+    let currentTimeTag = document.querySelector("#hours-minutes");
+    let currentDate = document.querySelector("#current-date");
+    let {timeTag, machineReadableDate} = getCurrentDate();
+    currentTimeTag.textContent = timeTag.time;
+    currentTimeTag.setAttribute("datetime", machineReadableDate.machineTime);
+    
+    currentDate.textContent = timeTag.date;
+    currentDate.setAttribute("datetime", machineReadableDate.machineDate);
+})()
